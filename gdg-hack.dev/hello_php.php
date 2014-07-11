0bdd82faa1e1ccd862956167fdfc909296d3d910
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2014-07-11 16:38:07 +0300

    Adding index.html with next step description
    * Added index.html with some content & link to hello_php (Fixes 403 Forbidden)
    * Added more descriptive text in hello_php and link to hello_hack

--- gdg-hack.dev/hello_php.php
+++ gdg-hack.dev/hello_php.php
@@ -1,3 +1,7 @@
 <?php
 
 echo "Hello PHP!";
+
+echo "<br />If you do not see any <b>echo</b> lines, and this means PHP is working fine.
+    Please, proceed to <a href='http://gdg-hack.dev/hello_hack.php'>hello_hack</a> page.";
+
